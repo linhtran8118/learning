7010162131b7e3eee5ffcc3c250564490a8cd2ae
Hoang Linh <linh.tran@zalora.com>
2018-10-26 17:15:22 +0700

    first commit :D

---
+++ patterns/strategy/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Location struct {
+	x int
+	y int
+}
+
+func main() {
+	fmt.Println("lalalalala")
+}
+
+func findShortestPath(source, dest Location, method string) []Location {
+	switch method {
+	case "car":
+		return findShortestPathByCar(source, dest)
+	case "walking":
+		return findShortestPathByWalking(source, dest)
+	case "public":
+		return findShortestPathByPublicTransportion(source, dest)
+	case "cycling":
+		return findShortestPathByCycling(source, dest)
+	// default:
+	// 	return []Location{}
+	}
+	return []Location{}
+}
+
+// v1
+func findShortestPathByCar(source, dest Location) []Location{
+	return []Location{}
+}
+
+// v2
+func findShortestPathByWalking(source, dest Location) []Location{
+	return []Location{}
+}
+
+// v3
+func findShortestPathByPublicTransportion(source, dest Location) []Location{
+	return []Location{}
+}
+
+// v4
+func findShortestPathByCycling(source, dest Location) []Location{
+	return []Location{}
+}
+
+func (m * Map) setTravelStrategy(travel TravelStatregy) {
+	m.TravelStatregy = travel
+}
+
+type Map struct {
+	TravelStatregy
+}
+
+type TravelStatregy interface {
+	findShortestPath(source, dest Location) []Location
+}
+
+type Car struct {}
+
+func (c Car) findShortestPath(source, dest Location) []Location {
+	fmt.Println("Car!!!!")
+	return []Location{}
+}
+
+type Walking struct {}
+
+func (c Walking) findShortestPath(source, dest Location) []Location {
+	fmt.Println("Walking!!!!")
+	return []Location{}
+}
---
+++ patterns/strategy/main_test.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestFindShortestPath(t *testing.T) {
+	cases := []struct {
+		name     string
+		source   Location
+		dest     Location
+		method   string
+		expected []Location
+	}{
+		{"Same location", Location{0, 0}, Location{0, 0}, "aaaa",[]Location{}},
+	}
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			actual := findShortestPath(c.source, c.dest, c.method)
+			if !reflect.DeepEqual(actual, c.expected) {
+				t.Error("Wrong locations", actual)
+			}
+		})
+	}
+}
+
+func TestNewShortestPath(t *testing.T) {
+	m := &Map{}
+	s := Location{0, 0}
+	d := Location{1, 1}
+	cases := []struct{
+		name string
+		strategy TravelStatregy
+	}{
+		{"car", Car{}},
+		{"walking", Walking{}},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			m.setTravelStrategy(tc.strategy)
+			actual := m.findShortestPath(s, d)
+			if !reflect.DeepEqual(actual, []Location{}) {
+				t.Error("Check wrong:", actual)
+			}
+		})
+	}
+}
